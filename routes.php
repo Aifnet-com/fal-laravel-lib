96e7a345d7a452c7c26ae5e3c95d02d2d0f6a8e1
alexchakmakov <aleksandar.chakmakov@aifnet.com>
2025-10-20 10:53:27 +0300

    Fix route to use FalController for file downloads

--- routes.php
+++ routes.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Aifnet\Fal\Http\Controllers\FalWebhookController;
+use Aifnet\Fal\Http\Controllers\FalController;
 
 Route::post('/fal/webhook', [FalWebhookController::class, 'handle'])->name('fal.webhook');
-Route::get('/fal/download/{falRequestId}', [FalWebhookController::class, 'download'])->name('fal.download');
+Route::get('/fal/download/{falRequestId}', [FalController::class, 'download'])->name('fal.download');
