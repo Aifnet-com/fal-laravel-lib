9eea5b3331a741ea796ddc6bc72fcd52278343a0
alexchakmakov <aleksandar.chakmakov@aifnet.com>
2025-10-21 15:35:54 +0300

    Add methods for managing input and output values in FalData model

--- src/Models/FalData.php
+++ src/Models/FalData.php
@@ -3,6 +3,7 @@
 namespace Aifnet\Fal\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class FalData extends Model
 {
@@ -21,5 +22,51 @@ class FalData extends Model
     public function request()
     {
         return $this->hasOne(FalRequest::class, 'data_id', 'id');
+    }
+
+    public function getInputValue($key, $default = null)
+    {
+        return Arr::get($this->input, $key, $default);
+    }
+
+    public function setInputValue($key, $value)
+    {
+        $input = $this->getAttribute('input') ?? [];
+        Arr::set($input, $key, $value);
+        $this->setAttribute('input', $input);
+
+        return $this;
+    }
+
+    public function removeInputValue($key)
+    {
+        $input = $this->getAttribute('input') ?? [];
+        Arr::forget($input, $key);
+        $this->setAttribute('input', $input);
+
+        return $this;
+    }
+
+    public function getOutputValue($key, $default = null)
+    {
+        return Arr::get($this->output, $key, $default);
+    }
+
+    public function setOutputValue($key, $value)
+    {
+        $output = $this->getAttribute('output') ?? [];
+        Arr::set($output, $key, $value);
+        $this->setAttribute('output', $output);
+
+        return $this;
+    }
+
+    public function removeOutputValue($key)
+    {
+        $output = $this->getAttribute('output') ?? [];
+        Arr::forget($output, $key);
+        $this->setAttribute('output', $output);
+
+        return $this;
     }
 }
